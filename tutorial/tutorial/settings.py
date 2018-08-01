5961068a24a35a2280014c8ec70d50c3f01f387a
xiahuagui <xiahuagui@ivymobile.com>
2018-08-01 14:45:50 +0800

    u

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -20,6 +20,7 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
+LOG_LEVEL = ERROR
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
