d41b7b993469f332996c65ea4a0da1e952ba40a2
xiahuagui <xiahuagui@ivymobile.com>
2018-08-01 14:42:48 +0800

    u

--- tutorial/tutorial/spiders/huxiu_spider.py
+++ tutorial/tutorial/spiders/huxiu_spider.py
@@ -15,14 +15,18 @@ class HuxiuSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        print(response)
+        print(type(response))
         for sel in response.xpath('//div[@class="mod-info-flow"]/div[@class="clearfix"]'):
-            item = HuxiuItem()
-            item['pic'] = sel.xpath('div[@class="mod-thumb"]/a/img/@src')
-            item['url'] = sel.xpath('div[@class="mod-thumb"]/a/@href')
-            url = response.urljoin(item['url'])
-            item['title'] = sel.xpath('div[@class="mob-ctt"]/h2/a/text()')
-            item['desc'] = sel.xpath('div[@class="mob-ctt"]/div[@class="mob-sub"]/text()')
-            print(item['title'],item['url'],item['desc'],item['pic'])
+            print(type(sel.xpath('div[@class="mod-thumb"]/a/img/@src')))
+            print(sel.xpath('div[@class="mod-thumb"]/a/img/@src'))
+            # item = HuxiuItem()
+            # item['pic'] = sel.xpath('div[@class="mod-thumb"]/a/img/@src')
+            # item['url'] = sel.xpath('div[@class="mod-thumb"]/a/@href')
+            # url = response.urljoin(item['url'])
+            # item['title'] = sel.xpath('div[@class="mob-ctt"]/h2/a/text()')
+            # item['desc'] = sel.xpath('div[@class="mob-ctt"]/div[@class="mob-sub"]/text()')
+            # print(item['title'],item['url'],item['desc'],item['pic'])
 
 
         # for sel in response.xpath('//div[@class="mod-info-flow"]/div/div[@class="mob-ctt"]'):
