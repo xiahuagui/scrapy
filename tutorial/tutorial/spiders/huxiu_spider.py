25da528a2e499382a8182060d43b77c0eb4de063
xiahuagui <xiahuagui@ivymobile.com>
2018-08-01 15:13:18 +0800

    u

--- tutorial/tutorial/spiders/huxiu_spider.py
+++ tutorial/tutorial/spiders/huxiu_spider.py
@@ -28,10 +28,10 @@ class HuxiuSpider(scrapy.Spider):
         #     print(item['title'],item['url'],item['desc'],item['pic'])
 
 
-        # for sel in response.xpath('//div[@class="mod-info-flow"]/div/div[@class="mob-ctt"]'):
-        #     item = HuxiuItem()
-        #     item['title'] = sel.xpath('h2/a/text()')[0].extract()
-        #     item['url'] = sel.xpath('h2/a/@href')[0].extract()
-        #     url = response.urljoin(item['url'])
-        #     item['desc'] = sel.xpath('div[@class="mob-sub"]/text()')[0].extract()
-        #     print(item['title'],item['url'],item['desc'])
+        for sel in response.xpath('//div[@class="mod-info-flow"]/div/div[@class="mob-ctt"]'):
+            item = HuxiuItem()
+            item['title'] = sel.xpath('h2/a/text()')[0].extract()
+            item['url'] = sel.xpath('h2/a/@href')[0].extract()
+            url = response.urljoin(item['url'])
+            item['desc'] = sel.xpath('div[@class="mob-sub"]/text()')[0].extract()
+            print(item['title'],item['url'],item['desc'])
