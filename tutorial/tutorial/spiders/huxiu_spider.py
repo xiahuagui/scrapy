89de44c542433115d7fa364b9d2f2ef1ea4e3ac8
xiahuagui <xiahuagui@ivymobile.com>
2018-08-01 16:25:38 +0800

    u

--- tutorial/tutorial/spiders/huxiu_spider.py
+++ tutorial/tutorial/spiders/huxiu_spider.py
@@ -10,6 +10,7 @@ from scrapy.http import Request, FormRequest, HtmlResponse
 #from scrapy_splash import SplashRequest,SplashMiddleware
 
 class HuxiuSpider(scrapy.Spider):
+    print('aaaaaaaaaaaaaaaaaaaaaa')
     name = "huxiu"
     allowed_domains = ["vc.cn"]
     #start_urls = "https://www.vc.cn/investments"
@@ -18,6 +19,7 @@ class HuxiuSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        print('bbbbbbbbbbbbbbbbbbbbbbbbb')
         for sel in response.xpath('//tbody[@class="investment-list"]/tr'):
             item = HuxiuItem()
             item['title'] = sel.xpath('div[@class="info"]/div[@class="name"]/a/text()')[0].extract()
