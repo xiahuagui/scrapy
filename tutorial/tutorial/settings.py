9e7097984bdfcae024861f9fd75bf06af7d2532f
xiahuagui <xiahuagui@ivymobile.com>
2018-08-01 16:15:11 +0800

    u

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -34,7 +34,7 @@ LOG_LEVEL = "INFO"
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
