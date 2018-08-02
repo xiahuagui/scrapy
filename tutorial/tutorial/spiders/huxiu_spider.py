cb02fa56f89f4d4eda2332c0335c22e5417f8cda
xiahuagui <xiahuagui@ivymobile.com>
2018-08-02 10:37:24 +0800

    u

--- tutorial/tutorial/spiders/huxiu_spider.py
+++ tutorial/tutorial/spiders/huxiu_spider.py
@@ -9,7 +9,6 @@ import scrapy
 from scrapy.http import Request, FormRequest, HtmlResponse
 
 class HuxiuSpider(scrapy.Spider):
-    print('aaaaaaaaaaaaaaaaaaaaaa')
     name = "huxiu"
     allowed_domains = ["vc.cn"]
     #start_urls = "https://www.vc.cn/investments"
@@ -25,9 +24,8 @@ class HuxiuSpider(scrapy.Spider):
             url = sel.xpath('td[@class="cover-info"]/div[@class="avatar square"]/a/@href').extract()[0]
             item['url'] = response.urljoin(url)
             item['pic'] = sel.xpath('td[@class="cover-info"]/div[@class="avatar square"]/a/img/@data-echo').extract()[0]
-            print(item['title'])
-            print(item['url'])
-            print(item['pic'])
+            yield item
+            
 
     # def start_requests(self):
     #     yield Request(url = self.start_urls, callback = self.parse)
