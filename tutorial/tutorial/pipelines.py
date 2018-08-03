271be13905b22cee57b8a88249a27930c35bae97
xiahuagui <xiahuagui@ivymobile.com>
2018-08-03 11:28:02 +0800

    u

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -4,8 +4,26 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from tutorial.items import HuxiuItem
+import pymysql.cursors
 
 
-class TutorialPipeline(object):
+class HuxiuPipeline(object):
+	def __init__(self):
+		# 连接数据库
+		connect = pymysql.Connect(
+		    host='localhost',
+		    port=3306,
+		    user='dev',
+		    passwd='1fi923^a3bui*9',
+		    db='my_data',
+		    charset='utf8'
+		)
+		# 获取游标
+		cursor = connect.cursor()
+
+
     def process_item(self, item, spider):
-        return item
+    	print(item)
+    	print(type(item))
+        #return item
