7e9394f04f7e076576612eb183df0f0adcc7accf
xiahuagui <xiahuagui@ivymobile.com>
2018-08-03 11:55:09 +0800

    u

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from tutorial.items import HuxiuItem
-import pymysql
+import pymysql.cursors
 
 
 class HuxiuPipeline(object):
