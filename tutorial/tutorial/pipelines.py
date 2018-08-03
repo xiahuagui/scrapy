d76bfa794fe7ffb940ed3c4b3659c664f09fe5c8
xiahuagui <xiahuagui@ivymobile.com>
2018-08-03 11:50:24 +0800

    u

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -22,7 +22,6 @@ class HuxiuPipeline(object):
 		# 获取游标
 		self.cursor = connect.cursor()
 
-
     def process_item(self, item, spider):
     	data = (item["title"].encode("utf-8"),item["url"].encode("utf-8"),item["pic"].encode("utf-8"))
     	sql = "INSERT INTO vc_data (title, url, pic) VALUES ( '%s', '%s', '%s' )"
