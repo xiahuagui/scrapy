1764357a731d0288a2a2467392270ab833eb05a7
夏化贵 <xiahuagui@ivymobile.com>
2018-11-19 23:09:04 +0800

    u

--- py/test5.py
+++ py/test5.py
@@ -5,7 +5,7 @@ from imutils.perspective import four_point_transform
 
 def main():
 	#读入图片
-	image = cv2.imread("/usr/www/scrapy/py/xin.jpg")
+	image = cv2.imread("./xin.jpg")
 	#转换为灰度图像
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -27,14 +27,14 @@ def main():
 	#这一步可有可无，主要是增加一圈白框，以免刚好卷子边框压线后期边缘检测无果。好的样本图就不用考虑这种问题
 	blurred = cv2.copyMakeBorder(blurred,5,5,5,5,cv2.BORDER_CONSTANT,value=(255,255,255))
 
-	cv2.imwrite('/usr/www/scrapy/py/11.jpg', blurred)
+	cv2.imwrite('./11.jpg', blurred)
 
 
 
 	#canny边缘检测
 	edged = cv2.Canny(blurred, 10, 100)
 
-	cv2.imwrite('/usr/www/scrapy/py/11_1.jpg', edged)
+	cv2.imwrite('./11_1.jpg', edged)
 	# 从边缘图中寻找轮廓，然后初始化答题卡对应的轮廓
 	'''
 	findContours
@@ -55,11 +55,10 @@ def main():
 
 
 	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-	# cnts1 = cnts[1] if imutils.is_cv2() else cnts[2]
 
 
-	docCnt = None
-	docCnt1 = None
+	docCnt = None  #答题区
+	docCnt1 = None #准考证号码
 	ss = 0
 	# 确保至少有一个轮廓被找到
 	if len(cnts) > 0:
@@ -77,38 +76,13 @@ def main():
 	                docCnt = approx
 	            else:
 	                docCnt1 = approx
-
-	            #break
 	        elif ss > 1:
 	            break
 	elif len(cnts)<=0:
-		print("未找到答题卡轮廓，重新扫描\n")
+		print("答题卡不符合规范，清重新扫描\n")
 		return
 
 
-	# docCnt1 = None
-	# # 确保至少有一个轮廓被找到
-	# if len(cnts1) > 0:
-	#     # 将轮廓按大小降序排序
-	#     cnts1 = sorted(cnts1, key=cv2.contourArea, reverse=True)
-	#     # 对排序后的轮廓循环处理
-	#     for c in cnts1:
-	#         # 获取近似的轮廓
-	#         peri = cv2.arcLength(c, True)
-	#         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-	#         # 如果近似轮廓有四个顶点，那么就认为找到了答题卡
-	#         if len(approx) == 4:
-	#             docCnt1 = approx
-	#             break
-	# elif len(cnts1)<=0:
-	# 	print("未找到答题卡轮廓，重新扫描\n")
-	# 	return
-
-	# print("aaaaaaaaaaaaaaa")
-	# print(len(cnts))
-	# print(docCnt)
-	# print(len(docCnt))
-	# return
 
 	newimage = image.copy()
 	for i in docCnt:
@@ -119,24 +93,17 @@ def main():
 	    #circle函数为在图像上作图，新建了一个图像用来演示四角选取
 		cv2.circle(newimage, (i[0][0],i[0][1]), 50, (255, 0, 0), -1)
 
-	cv2.imwrite('/usr/www/scrapy/py/22.jpg', newimage)
+	cv2.imwrite('./22.jpg', newimage)
+	check_choice_question(image,gray,docCnt)  #答题区域
+	check_admission_ticket(image,gray,docCnt1)
 
-	return 
-
-
+def check_admission_ticket(image,gray,docCnt):
 	paper = four_point_transform(image, docCnt.reshape(4, 2))
 	warped = four_point_transform(gray, docCnt.reshape(4, 2))
-
-	cv2.imwrite('/usr/www/scrapy/py/33.jpg', paper)
-	cv2.imwrite('/usr/www/scrapy/py/44.jpg', warped)
-
-
 	# 对灰度图应用二值化算法
 	thresh=cv2.adaptiveThreshold(warped,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,53,2)
-
-	width1 = 2400
-	height1 = 2800
-
+	width1 = 1140
+	height1 = 1040
 	#重塑可能用到的图像
 	thresh = cv2.resize(thresh, (width1, height1), cv2.INTER_LANCZOS4)
 	paper = cv2.resize(paper, (width1, height1), cv2.INTER_LANCZOS4)
@@ -146,8 +113,75 @@ def main():
 	#二进制二值化
 	ChQImg = cv2.threshold(ChQImg, 100, 225, cv2.THRESH_BINARY)[1]
 
-	cv2.imwrite('/usr/www/scrapy/py/55.jpg', thresh)
-	cv2.imwrite('/usr/www/scrapy/py/66.jpg', ChQImg)
+	cv2.imwrite('./55.jpg', thresh)
+	cv2.imwrite('./66.jpg', ChQImg)
+
+	cnts = cv2.findContours(ChQImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	Answer = []
+	for c in cnts:
+	     # 计算轮廓的边界框，然后利用边界框数据计算宽高比
+	      (x, y, w, h) = cv2.boundingRect(c)
+	      if w > 62 and w < 108 and h > 18 and h < 45 and y>235 and y<1006:
+	            M = cv2.moments(c)
+	            cX = int(M["m10"] / M["m00"])
+	            cY = int(M["m01"] / M["m00"])
+	            #绘制中心及其轮廓
+	            cv2.drawContours(paper, c, -1, (0, 0, 255), 5, lineType=0)
+	            cv2.circle(paper, (cX, cY), 7, (255, 255, 255), -1)
+	            #保存选中模块的中心坐标
+	            Answer.append((cX, cY))
+
+	cv2.imwrite('./88.jpg', paper)   
+
+	xt1 = [17, 140, 269, 396, 521, 649, 772, 900, 1024, 1129]  #选项左侧x坐标 10
+	yt1 = [231, 311, 390, 473, 554, 635, 715, 793, 873, 953, 1016] #11
+
+	IDAnswer = {}
+	for i in Answer:
+	    for j in range(0,len(xt1)-1):
+	        if i[0]>xt1[j] and i[0]<xt1[j+1]:
+	            for k in range(0,len(yt1)-1):
+	                if i[1]>yt1[k] and i[1]<yt1[k+1]:
+	                    rs = judge1(j,k)
+	                    if rs[0] in IDAnswer:
+	                    	print(int(j)+1, "项学号选了多个值\n")
+	                    	IDAnswer[rs[0]] = rs[1]
+	                    else:
+	                    	IDAnswer[rs[0]] = rs[1]
+
+	result = ""
+	for key in range(0, 9): #之后改为总题数 没写的自然就定为：题号：空
+		if key not in IDAnswer:
+			result = result+"x"
+			continue
+		result = result + str(IDAnswer[key])
+		
+
+	print(result)
+
+
+def judge1(x,y):
+	return (x, y)
+
+
+def check_choice_question(image,gray,docCnt):
+	paper = four_point_transform(image, docCnt.reshape(4, 2))
+	warped = four_point_transform(gray, docCnt.reshape(4, 2))
+	# 对灰度图应用二值化算法
+	thresh=cv2.adaptiveThreshold(warped,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,53,2)
+	width1 = 2400
+	height1 = 2800
+	#重塑可能用到的图像
+	thresh = cv2.resize(thresh, (width1, height1), cv2.INTER_LANCZOS4)
+	paper = cv2.resize(paper, (width1, height1), cv2.INTER_LANCZOS4)
+	warped = cv2.resize(warped, (width1, height1), cv2.INTER_LANCZOS4)
+	#均值滤波
+	ChQImg = cv2.blur(thresh, (23, 23))
+	#二进制二值化
+	ChQImg = cv2.threshold(ChQImg, 100, 225, cv2.THRESH_BINARY)[1]
+	cv2.imwrite('./55.jpg', thresh)
+	cv2.imwrite('./66.jpg', ChQImg)
 	'''
 	    threshold参数说明
 	    第一个参数 src    指原图像，原图像应该是灰度图。
@@ -160,38 +194,28 @@ def main():
 	                •cv2.THRESH_TOZERO        
 	                •cv2.THRESH_TOZERO_INV    
 	'''
-
-
 	# 在二值图像中查找轮廓
 	cnts = cv2.findContours(ChQImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 	Answer = []
-
 	for c in cnts:
 	     # 计算轮廓的边界框，然后利用边界框数据计算宽高比
 	      (x, y, w, h) = cv2.boundingRect(c)
-	      if (w > 60 & h > 20)and y>900 and y<2000:
+	      if w > 62 and h > 22 and y>90 and y<2700:
 	            M = cv2.moments(c)
 	            cX = int(M["m10"] / M["m00"])
 	            cY = int(M["m01"] / M["m00"])
-
-
 	            #绘制中心及其轮廓
 	            cv2.drawContours(paper, c, -1, (0, 0, 255), 5, lineType=0)
 	            cv2.circle(paper, (cX, cY), 7, (255, 255, 255), -1)
-
 	            #保存选中模块的中心坐标
 	            Answer.append((cX, cY))
 
-	cv2.imwrite('/usr/www/scrapy/py/77.jpg', paper)
+	cv2.imwrite('./77.jpg', paper)
 	        
 
-	print(Answer)
-	print(len(Answer))
-	return
-
-	xt1 = [69, 132, 255, 378, 501, 651, 723, 846, 969, 1092, 1233, 1314, 1437, 1560, 1680, 1824, 1905, 2028, 2154, 2280, 2370]  #选项左侧x坐标 21
-	yt1 = [948, 1017, 1089, 1155, 1227, 1317, 1383, 1455, 1524, 1593, 1683, 1752, 1821, 1893, 1962, 2001]                       #选项上测y坐标 16
+	xt1 = [15, 120, 225, 330, 435, 645, 747, 852, 954, 1059, 1260, 1365, 1470, 1572, 1674, 1878, 1983, 2088, 2190, 2295, 2376]  #选项左侧x坐标 21
+	yt1 = [96, 192, 270, 351,429, 627, 735, 819, 900, 978, 1176, 1290, 1374, 1452, 1533, 1725, 1836, 1920, 1998, 2079, 2274, 2385, 2469, 2550, 2631, 2730] #选项上测y坐标 26
 
 	IDAnswer = {}
 	for i in Answer:
@@ -210,11 +234,8 @@ def main():
 	                    	IDAnswer[rs[0]] = []
 	                    	IDAnswer[rs[0]].append(rs[1])
 
-	if len(IDAnswer) <= 0:
-		print("白卷，没有任何结果\n")
-		return
 	result = {}
-	for key in range(1, len(IDAnswer)+1): #之后改为总题数 没写的自然就定为：题号：空
+	for key in range(1, 100+1): #之后改为总题数 没写的自然就定为：题号：空
 		if key not in IDAnswer:
 			result[key] = ""
 			continue
@@ -224,37 +245,46 @@ def main():
 	print(result)
 	print(len(result))
 
-def judgey0(y):
-    if (y / 5 < 1):
-        return  y + 1
-    elif y / 5 < 2 and y/5>=1:
-        return y % 5 + 20 + 1
-    else:
-        return y % 5 + 40 + 1
+#题号
 def judgex0(x):
-    if(x%5==1):
+    if (x / 5 < 1):
+        return  x + 1
+    elif x / 5 < 2 and x/5>=1:
+        return x % 5 + 5 + 1
+    elif x / 5 < 3 and x/5>=2:
+ 	    return x % 5 + 10 + 1
+    else:
+        return x % 5 + 15 + 1
+
+def judgey0(y):
+    if(y%5==1):
         return 'A'
-    elif(x%5==2):
+    elif(y%5==2):
         return 'B'
-    elif(x%5==3):
+    elif(y%5==3):
         return 'C'
-    elif(x%5==4):
+    elif(y%5==4):
         return 'D'
-    elif(x%5==0):  #如果是题号 返回空
-    	return ''
+    elif(y%5==0):  #如果是题号 返回空
+        return ''
+
 def judge0(x,y):
-    if x/5<1 :
+    if y/5<1 :
         #print(judgey0(y))
-        return (judgey0(y),judgex0(x))
-    elif x/5<2 and x/5>=1:
+        return (judgex0(x),judgey0(y))
+    elif y/5<2 and y/5>=1:
         #print(judgey0(y)+5)
-        return (judgey0(y)+5,judgex0(x))
-    elif x/5<3 and x/5>=2:
-       # print(judgey0(y)+10)
-        return (judgey0(y)+10,judgex0(x))
+        return (judgex0(x)+20,judgey0(y))
+    elif y/5<3 and y/5>=2:
+        # print(judgey0(y)+10)
+        return (judgex0(x)+40,judgey0(y))
+    elif y/5<4 and y/5>=3:
+        # print(judgey0(y)+10)
+        return (judgex0(x)+60,judgey0(y))
     else:
         #print(judgey0(y)+15)
-        return (judgey0(y)+15,judgex0(x))
+        return (judgex0(x)+80,judgey0(y))
+
 
 if __name__ == "__main__":
 	main()
