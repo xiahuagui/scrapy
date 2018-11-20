c2781718bea0fa46a44df146175de2d4f944633f
xiahuagui <xiahuagui@ivymobile.com>
2018-11-20 11:57:35 +0800

    u

--- py/server.py
+++ py/server.py
@@ -1,8 +1,11 @@
-def application(environ, start_response):
-    status = '200 OK'
-    output = 'Hello World!'
+# server.py
+# 从wsgiref模块导入:
+from wsgiref.simple_server import make_server
+# 导入我们自己编写的application函数:
+from hello import application
 
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output]
+# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+# 开始监听HTTP请求:
+httpd.serve_forever()
